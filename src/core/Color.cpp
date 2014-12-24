6f0402b46b1260d0b8f75d32d81b71cd006b8cda
Shane del Solar <sads3@gatech.edu>
2014-12-24 13:59:57 -0500

    Adding Color Class.  Small typo fix in TgaLoader.

---
+++ src/core/Color.cpp
@@ -0,0 +1,42 @@
+#include "core/Color.hpp"
+
+using namespace Core;
+
+Color::Color()
+{
+	this->set(0.0f);
+}
+
+Color::Color(float all)
+{
+	this->set(all);
+}
+
+Color::Color(float red  , float green, float blue)
+{
+	this->set(red, green, blue);
+}
+
+Color::Color(float red  , float green, float blue, float alpha)
+{
+	this->set(red, green, blue, alpha);
+}
+
+
+void Color::set(float all)
+{
+	this->set(all, all, all, all);
+}
+
+void Color::set(float red  , float green, float blue)
+{
+	this->set(red, green, blue, 1.0f);
+}
+
+void Color::set(float red  , float green, float blue , float alpha)
+{
+	this->red   = red;
+	this->green = green;
+	this->blue  = blue;
+	this->alpha = alpha;
+}
